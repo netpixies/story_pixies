feb5b450c107f3b8a6307798cf0545ef73db6f50
Alyssa Hardy <ahardy@paloaltonetworks.com>
2018-10-17 22:56:52 -0700

    Basic layout

--- storypixies.py
+++ storypixies.py
@@ -1,37 +1,41 @@
 import kivy
 
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
+
 kivy.require('1.10.1')
 
 
-class StoryCreator(Widget):
+class StoryHome(Screen):
     pass
 
 
-class StoryLibrary(Widget):
+class StoryLibrary(Screen):
     pass
 
 
-class StoryBook(Widget):
+class StoryCreator(Screen):
     pass
 
+class StoryBook(Screen):
+    pass
 
 class StoryBase(BoxLayout):
-    def get_title(self):
-        return 'Story base title'
+    pass
 
-    def create_story(self):
-        pass
-
+class StorySettings(Screen):
+    pass
 
 class StoryPixiesApp(App):
 
     def build(self):
-        story_base = StoryBase()
-        self.title = 'Welcome to Story Pixies!'
-        return story_base
+        return StoryBase()
 
     def build_config(self, config):
         pass
