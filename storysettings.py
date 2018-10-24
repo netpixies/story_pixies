9a1aead91fd83681133bb2791e4734bf015e19e9
Alyssa Hardy <ahardy@paloaltonetworks.com>
2018-10-23 21:52:27 -0700

    Update configs

--- storysettings.py
+++ storysettings.py
@@ -1,28 +1,39 @@
 import json
 
 def get_settings_json(name):
-    settings_json = [
+    if name == 'global':
+        settings_json = [
         {'type': 'title',
-         'title': 'Story Settings'},
-        {'type': 'string',
-         'title': 'Library Name',
-         'desc': 'The library name',
-         'section': 'libraryname',
-         'key': 'name'},
+         'title': 'Global Settings'},
         {'type': 'path',
-         'title': 'Library Stories',
-         'desc': 'Where are the stories located?',
-         'section': 'libraryname',
-         'key': 'story_dir',
-         'dirselect': True},
-        {'type': 'library_options',
-         'title': 'Current Book',
-         'desc': 'The Current Book',
-         'section': 'libraryname',
-         'key': 'current_book'}]
+         'title': 'Template Location',
+         'desc': 'The template directory',
+         'section': 'global',
+         'key': 'template_dir',
+         'dirselect': True}]
+    else:
+        settings_json = [
+            {'type': 'title',
+             'title': 'Story Settings'},
+            {'type': 'string',
+             'title': 'Library Name',
+             'desc': 'The library name',
+             'section': 'libraryname',
+             'key': 'name'},
+            {'type': 'path',
+             'title': 'Library Stories',
+             'desc': 'Where are the stories located?',
+             'section': 'libraryname',
+             'key': 'story_dir',
+             'dirselect': True},
+            {'type': 'library_options',
+             'title': 'Current Book',
+             'desc': 'The Current Book',
+             'section': 'libraryname',
+             'key': 'current_book'}]
 
-    for idx, i in enumerate(settings_json):
-        if settings_json[idx].get('section'):
-            settings_json[idx]['section'] = name
+        for idx, i in enumerate(settings_json):
+            if settings_json[idx].get('section'):
+                settings_json[idx]['section'] = name
 
     return json.dumps(settings_json)
