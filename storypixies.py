bbc86d10bf217f82b0aa2219b9f22e8f62b93bf6
Alyssa Hardy <ahardy@paloaltonetworks.com>
2018-11-10 12:57:58 -0800

    Redsign for cleaner layout

--- storypixies.py
+++ storypixies.py
@@ -1,49 +1,37 @@
 import kivy
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.togglebutton import ToggleButton
+
+from pathlib import Path
+from functools import partial
+from storysettings import get_settings_json
 
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.settings import SettingsWithTabbedPanel
-from kivy.uix.screenmanager import Screen
-from storysettings import get_settings_json
-from kivy.uix.settings import SettingOptions
-from kivy.uix.settings import StringProperty, NumericProperty, ObjectProperty, DictProperty, ListProperty
-from pathlib import Path
 from kivy.config import ConfigParser
-from kivy.uix.button import Button
-from kivy.uix.image import Image
-from kivy.uix.videoplayer import VideoPlayer
+
+from kivy.properties import ObjectProperty, ListProperty, StringProperty, NumericProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from random import randint
-from kivy.uix.carousel import Carousel
+from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
+from kivy.uix.button import Button
 
 kivy.require('1.10.1')
 
 
-class LibraryOptions(SettingOptions):
-    function_string = StringProperty()
-
-    def _create_popup(self, instance):
-        story_list = (Path(__file__).parents[0].absolute() / "libraries" / instance.section).glob('**/*.ini')
-        self.options = [l.stem for l in story_list]
-        super(LibraryOptions, self)._create_popup(instance)
-
-
-class LibrarySettings(SettingsWithTabbedPanel):
-    def __init__(self, **kwargs):
-        super(LibrarySettings, self).__init__(**kwargs)
-        self.register_type('library_options', LibraryOptions)
+class MainMenu(GridLayout):
+    pass
 
 
 class LibraryButton(Button):
     pass
 
 
-class StoryHome(Screen):
+class Home(Screen):
     def __init__(self, **kwargs):
-        super(StoryHome, self).__init__(**kwargs)
+        super(Home, self).__init__(**kwargs)
 
-    def on_enter(self, *args):
+    def on_pre_enter(self, *args):
         library_grid = self.ids.library_grid
         library_grid.clear_widgets()
         for i in self.libraries:
@@ -53,120 +41,53 @@ class StoryHome(Screen):
         library_grid.bind(minimum_height=library_grid.setter('height'))
 
 
-class StoryLibrary(Screen):
-
-    def __init__(self, **kwargs):
-        super(StoryLibrary, self).__init__(**kwargs)
-
-    def on_pre_enter(self, *args):
-        self.ids.story_button.text = main_app.get_story_title()
-        self.ids.story_button_image.source = main_app.get_story_media()
-
-class StoryCreator(Screen):
-    pass
 
 
-class StoryBook(Screen):
-    media_property = ObjectProperty(None)
+class Library(Screen):
+    def on_enter(self, *args):
+        print "Entering library, shhhh!"
+    #def __init__(self, **kwargs):
+    #    super(Library, self).__init__(**kwargs)
 
-    def __init__(self, **kwargs):
-        super(StoryBook, self).__init__(**kwargs)
-
-    def on_pre_enter(self, *args):
-        self.assemble_layout()
-
-    def assemble_layout(self):
-        if self.media_property is not None:
-            self.media_property.state = 'stop'
-
-        top_grid = self.ids.story_book_grid
-        top_grid.clear_widgets()
-        d = self.get_story_display()
-        self.media_property = self.get_media_display()
-        story_back_button = Button(text="Back",
-                                   size_hint_y=0.1,
-                                   background_normal='images/backgrounds/button.png',
-                                   bold=True)
-        story_next_button = Button(text="Next",
-                                   size_hint_y=0.1,
-                                   background_normal='images/backgrounds/button.png',
-                                   bold=True)
-        story_back_button.bind(on_release=self.prev_page)
-        story_next_button.bind(on_release=self.next_page)
-        top_grid.add_widget(d)
-        top_grid.add_widget(self.media_property)
-        top_grid.add_widget(story_back_button)
-        top_grid.add_widget(story_next_button)
-
-    def prev_page(self, instance):
-        if self.current_page == 'title':
-            self.manager.current = 'story_library'
-        else:
-            main_app.previous_page()
-            self.assemble_layout()
-
-    def next_page(self, instance):
-        page_now = self.current_page_no
-        main_app.next_page()
-        if page_now == self.current_page_no:
-            self.manager.current = 'story_library'
-        else:
-            self.assemble_layout()
-
-    @staticmethod
-    def get_story_display():
-        story_text_label = Label(text=main_app.get_story_text(),
-                                 text_size=(None, None),
-                                 font_size="20sp",
-                                 pos_hint={'center_x': 0.5, 'center_y': 100.85},
-                                 size_hint_y=1,
-                                 halign="center",
-                                 valign="middle")
-
-        return story_text_label
-
-    def get_media_display(self):
-        media_type = main_app.get_story_media_type()
-        if media_type == 'image':
-            return Image(source=main_app.get_story_media(), allow_stretch=False, keep_ratio=True)
-        elif media_type == 'video':
-            return VideoPlayer(id=self.current_page + 'video', source=main_app.get_story_media(), state='play',
-                               options={'allow_stretch': True, 'keep_ratio': True})
+    #def on_pre_enter(self, *args):
+    #    print "Pre enter"
+    #    self.ids.story_button.text = self.main_app.get_story_title()
+    #    self.ids.story_button_image.source = self.main_app.get_story_media()
+    #    print self.ids.story_button.text
 
 
-class StoryBase(BoxLayout):
-    pass
+
+class Story(Screen):
+    def on_enter(self, *args):
+        print "Entering Story"
 
 
-class StorySettings(Screen):
-    pass
-
-
-class StoryFront(Screen):
-    pass
-
-
-class StoryTemplate(BoxLayout):
-    pass
-
-
-class CustomData(BoxLayout):
-    pass
+class Creator(Screen):
+    def on_enter(self, *args):
+        print "Entering Creator"
 
 
 class StoryPixiesApp(App):
-    selected_library = StringProperty(None)
+    manager = ObjectProperty(None)
+    home = ObjectProperty(None)
+    library = ObjectProperty(None)
+    creator = ObjectProperty(None)
+    story = ObjectProperty(None)
+    menu = ObjectProperty(None)
+    top_grid = ObjectProperty(None)
+
     libraries = ListProperty()
+    selected_library = StringProperty(None)
 
     stories = ListProperty()
-    selected_story_no = NumericProperty(0)
     selected_story = StringProperty(None)
-
-    template_config = ObjectProperty(None)
+    selected_story_no = NumericProperty(0)
 
     current_page = StringProperty("title")
     current_page_no = NumericProperty(0)
     current_pages = ListProperty()
+
+    template_config = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         """
@@ -177,7 +98,6 @@ class StoryPixiesApp(App):
         """
         super(StoryPixiesApp, self).__init__(**kwargs)
         self.libraries = self.get_libraries()
-
         self.selected_library = self.libraries[0]
 
     def set_property_defaults(self):
@@ -190,6 +110,76 @@ class StoryPixiesApp(App):
         self.current_page_no = 0
         self.current_pages = []
 
+    def build(self):
+        self.title = 'Story Pixies'
+        self.manager = ScreenManager(transition=FadeTransition())
+
+        # Add a home screen where the user can change libraries
+        self.home = Home(name='home')
+        self.manager.add_widget(self.home)
+
+        # Add a library screen where the user can browse their stories
+        self.library = Library(name='library')
+        self.manager.add_widget(self.library)
+
+        # Add a stories screen where the user can read stories
+        self.story = Story(name='story')
+        self.manager.add_widget(self.story)
+
+        # Add a creator screen where the user can create new stories and templates
+        self.creator = Creator(name='creator')
+        self.manager.add_widget(self.creator)
+
+        self.menu = MainMenu()
+        self.top_grid = GridLayout(cols=1)
+        self.top_grid.add_widget(self.menu)
+        self.top_grid.add_widget(self.manager)
+
+        # Initialize the selected library
+        self.set_selected_library(self.selected_library)
+
+        return self.top_grid
+
+    def build_config(self, config):
+        config.setdefaults('global', {'template_dir': Path(__file__).parents[0].absolute() / "templates"})
+        for library in self.libraries:
+            config.setdefaults(library, {
+                'name': library,
+                'story_dir': Path(__file__).parents[0].absolute() / "libraries" / library,
+                'current_book': ''
+            })
+
+    def build_settings(self, settings):
+        settings.add_json_panel('Global', self.config, data=get_settings_json('global'))
+        for library in self.libraries:
+            settings.add_json_panel(library.capitalize(), self.config, data=get_settings_json(library))
+
+    def on_config_change(self, config, section, key, value):
+        print config, section, key, value
+
+    def home_screen(self, button):
+        button = self.menu.homebutton
+        self.switch_screen('home', button)
+
+    def library_screen(self, _):
+        button = self.menu.librarybutton
+        self.switch_screen('library', button)
+
+    def story_screen(self, _):
+        button = self.menu.storybutton
+        self.switch_screen('story', button)
+
+    def creator_screen(self, _):
+        button = self.menu.creatorbutton
+        self.switch_screen('creator', button)
+
+    def switch_screen(self, screen_name, button):
+        print "Switching"
+        #if self.manager and screen_name in self.manager.screen_names:
+        button.state = 'down'
+
+        self.manager.current = screen_name
+
     def set_selected_library(self, library):
         """
         Resets the defaults.
@@ -198,6 +188,7 @@ class StoryPixiesApp(App):
         Sets selected story to the first.
         :param library: the library to set as current
         """
+        print "Setting selected library to {}".format(library)
         if library in self.libraries:
             self.set_property_defaults()
             self.selected_library = library
@@ -274,29 +265,6 @@ class StoryPixiesApp(App):
         self.current_page_no = min(self.current_page_no - 1, len(self.current_pages) - 1)
         self.current_page = self.current_pages[self.current_page_no]
 
-    def build(self):
-        self.settings_cls = LibrarySettings
-        self.use_kivy_settings = False
-        self.title = 'Story Pixies'
-        return StoryBase()
-
-    def build_config(self, config):
-        config.setdefaults('global', {'template_dir': Path(__file__).parents[0].absolute() / "templates"})
-        for library in self.libraries:
-            config.setdefaults(library, {
-                'name': library,
-                'story_dir': Path(__file__).parents[0].absolute() / "libraries" / library,
-                'current_book': ''
-            })
-
-    def build_settings(self, settings):
-        settings.add_json_panel('Global', self.config, data=get_settings_json('global'))
-        for library in self.libraries:
-            settings.add_json_panel(library.capitalize(), self.config, data=get_settings_json(library))
-
-    def on_config_change(self, config, section, key, value):
-        print config, section, key, value
-
     @staticmethod
     def get_libraries():
         library_list = (Path(__file__).parents[0].absolute() / "libraries")
@@ -313,6 +281,7 @@ class StoryPixiesApp(App):
         return self.template_config.get(page, value)
 
     def get_story_title(self):
+        print "Getting title"
         return self.get_story_value('title', 'name')
 
     def get_story_media(self):
@@ -326,5 +295,4 @@ class StoryPixiesApp(App):
 
 
 if __name__ == '__main__':
-    main_app = StoryPixiesApp()
-    main_app.run()
+    StoryPixiesApp().run()
