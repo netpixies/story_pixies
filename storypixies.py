8e8d6cc345b715b00f4b517a2d3679ee8bbacc10
Alyssa Hardy <ahardy@paloaltonetworks.com>
2018-11-10 14:34:42 -0800

    Separate out stories and libraries into their own class. Add template name to library config

--- storypixies.py
+++ storypixies.py
@@ -10,7 +10,7 @@ from storysettings import get_settings_json
 from kivy.app import App
 from kivy.config import ConfigParser
 
-from kivy.properties import ObjectProperty, ListProperty, StringProperty, NumericProperty
+from kivy.properties import ObjectProperty, ListProperty, StringProperty, NumericProperty, DictProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
@@ -32,6 +32,8 @@ class Home(Screen):
         super(Home, self).__init__(**kwargs)
 
     def on_pre_enter(self, *args):
+        print "Welcome home!"
+
         library_grid = self.ids.library_grid
         library_grid.clear_widgets()
         for i in self.libraries:
@@ -40,32 +42,214 @@ class Home(Screen):
 
         library_grid.bind(minimum_height=library_grid.setter('height'))
 
-
-
+        if self.app.menu is not None:
+            self.app.menu.storybutton.state = 'normal'
+            self.app.menu.creatorbutton.state = 'normal'
+            self.app.menu.librarybutton.state = 'normal'
 
 class Library(Screen):
-    def on_enter(self, *args):
-        print "Entering library, shhhh!"
-    #def __init__(self, **kwargs):
-    #    super(Library, self).__init__(**kwargs)
 
-    #def on_pre_enter(self, *args):
+    def on_pre_enter(self):
+        """
+        Ensure the other buttons are unselected. For some
+        reason, multiple toggle buttons wind up selected.
+        """
+        print "Entering library, shhhh!"
     #    print "Pre enter"
     #    self.ids.story_button.text = self.main_app.get_story_title()
     #    self.ids.story_button_image.source = self.main_app.get_story_media()
     #    print self.ids.story_button.text
+        self.app.menu.homebutton.state = 'normal'
+        self.app.menu.creatorbutton.state = 'normal'
+        self.app.menu.storybutton.state = 'normal'
 
+class SingleLibrary(Button):
+    # Path object of the library directory
+    location = ObjectProperty(None)
+
+    # List of StoryBook objects in this library
+    stories = ListProperty()
+
+    # The name of this library
+    name = StringProperty(None)
+
+    # The currently set story, defaults to the first
+    current_story = NumericProperty(None)
+
+    def __init__(self, **kwargs):
+        super(SingleLibrary, self).__init__(**kwargs)
+        self.name = kwargs['name']
+        self.location = kwargs['location']
+        self.stories = []
+        self.current_story = 0
+        self.add_stories()
+
+    def add_stories(self):
+        i = 0
+        for story in self.location.iterdir():
+            if story.is_file():
+                self.stories.append(StoryBook(library=self.name,
+                                              title=story.stem,
+                                              location=story,
+                                              number=i))
+            i += 1
+
+    def set_current_story(self, num):
+        """
+        Set the currently running story.
+        :param num: The story number to set
+        :return: Return the same number
+        """
+        self.current_story = num
+        return num
+
+    def next_story(self):
+        """
+        Updates the selected story to be the next one.
+        :return: The name of the story
+        """
+        return self.set_current_story(self.selected_story_no + 1)
+
+    def prev_story(self):
+        """
+        Updates the selected story to be the previous one.
+        :return: The name of the story
+        """
+        return self.set_current_story(self.selected_story_no - 1)
 
 
 class Story(Screen):
-    def on_enter(self, *args):
+
+    def on_pre_enter(self):
+        """
+        Ensure the other buttons are unselected. For some
+        reason, multiple toggle buttons wind up selected.
+        """
         print "Entering Story"
+
+        self.app.menu.homebutton.state = 'normal'
+        self.app.menu.creatorbutton.state = 'normal'
+        self.app.menu.librarybutton.state = 'normal'
+
+
+class StoryBook(GridLayout):
+    # This story's title
+    title = StringProperty(None)
+
+    # The story's library
+    library_parent = StringProperty(None)
+
+    # The story's library number
+    story_number = NumericProperty(None)
+
+    # The current set page by name
+    current_page = StringProperty("title")
+
+    # The current set page by number
+    current_page_no = NumericProperty(0)
+
+    # List of all pages by name
+    pages = ListProperty()
+
+    # The name of this story's template
+    template = StringProperty(None)
+
+    # The parsed story config
+    story_config = ObjectProperty(None)
+
+    # Where is the title media?
+    title_media_location = StringProperty(None)
+
+    # What kind of media does the title have, image or video?
+    title_media = StringProperty(None)
+
+    def __init__(self, **kwargs):
+        """
+        Initialize starting values. Set kwargs values.
+        Set current page to title and page number to 0.
+        :param kwargs:
+        title: The title of this story
+        library: The name of this story's library
+        story_number: This story's library number
+        """
+        super(StoryBook, self).__init__(**kwargs)
+        self.title = kwargs['title']
+        self.library_parent = kwargs['library']
+        self.story_number = kwargs['number']
+
+        self.current_page = "title"
+        self.current_page_no = 0
+        self.pages = []
+
+        self.story_config = ConfigParser()
+
+    def load_story_config(self):
+        story_dir = (Path(__file__).parents[0].absolute() / "libraries" / self.library_parent)
+        tmp_config = ConfigParser()
+        tmp_config.read(str(story_dir) + '/' + self.title + '.ini')
+
+        # Get story template
+        self.template = tmp_config.get('template', 'name')
+
+        if not self.template.endswith('.ini'):
+            self.template = self.template + '.ini'
+
+        template_location = self.app.template_dir + '/' + self.template
+
+        # Set defaults from story .ini file
+        self.story_config.setall('DEFAULT', dict(tmp_config.items('values')))
+
+        # Set config from story's template. Values will be interpolated from above DEFAULT.
+        self.story_config.read(template_location)
+
+        # Find the media type (image, video) for this story's title page
+        self.title_media = self.story_config.get('title', 'media')
+
+        # Find the media location for this story's title page
+        self.title_media_location = self.story_config.get('title', 'media_location')
+
+        # Find all the pages
+        self.pages = ['title'] + [x.strip() for x in self.story_config.get('title', 'pages').split(',')]
+
+    def next_page(self):
+        """
+        Updates the current page to be the next page
+        """
+        self.current_page_no = min(self.current_page_no + 1, len(self.current_pages) - 1)
+        self.current_page = self.current_pages[self.current_page_no]
+
+    def previous_page(self):
+        """
+        Updates the current page to be the previous page
+        """
+        self.current_page_no = min(self.current_page_no - 1, len(self.current_pages) - 1)
+        self.current_page = self.current_pages[self.current_page_no]
+
+    def get_story_value(self, page, value):
+        return self.story_config.get(page, value)
+
+    def get_story_media(self):
+        return self.get_story_value(self.current_page, 'media_location')
+
+    def get_story_media_type(self):
+        return self.get_story_value(self.current_page, 'media')
+
+    def get_story_text(self):
+        return self.get_story_value(self.current_page, 'text')
 
 
 class Creator(Screen):
-    def on_enter(self, *args):
-        print "Entering Creator"
 
+    def on_pre_enter(self):
+        """
+        Ensure the other buttons are unselected. For some
+        reason, multiple toggle buttons wind up selected.
+        """
+        print "Welcome to the Creator!"
+
+        self.app.menu.homebutton.state = 'normal'
+        self.app.menu.storybutton.state = 'normal'
+        self.app.menu.librarybutton.state = 'normal'
 
 class StoryPixiesApp(App):
     manager = ObjectProperty(None)
@@ -76,18 +260,13 @@ class StoryPixiesApp(App):
     menu = ObjectProperty(None)
     top_grid = ObjectProperty(None)
 
-    libraries = ListProperty()
+    libraries = DictProperty()
     selected_library = StringProperty(None)
-
-    stories = ListProperty()
     selected_story = StringProperty(None)
     selected_story_no = NumericProperty(0)
 
-    current_page = StringProperty("title")
-    current_page_no = NumericProperty(0)
-    current_pages = ListProperty()
-
-    template_config = ObjectProperty(None)
+    template_dir = ObjectProperty(None)
+    library_dir = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         """
@@ -97,18 +276,34 @@ class StoryPixiesApp(App):
         :param kwargs:
         """
         super(StoryPixiesApp, self).__init__(**kwargs)
-        self.libraries = self.get_libraries()
-        self.selected_library = self.libraries[0]
 
-    def set_property_defaults(self):
+        # Initialize libraries from files
+        self.template_dir = (Path(__file__).parents[0].absolute() / "templates")
+        self.library_dir = (Path(__file__).parents[0].absolute() / "libraries")
+        self.add_libraries()
+
+    def add_libraries(self):
+        self.libraries = {}
+
+        for library in self.library_dir.iterdir():
+            print "Adding library {}".format(library.stem)
+            self.libraries[library.stem] = SingleLibrary(name=library.stem, location=library)
+
+        if len(self.libraries.keys()) == 0:
+            self.set_selected_library(None)
+        else:
+            self.set_selected_library(self.libraries.keys()[0])
+
+    def set_selected_library(self, library=None):
         """
-        Sets/resets the default values for properties
+        Stores passed library as selected.
+        :param library: the library to set as current
         """
-        self.selected_story_no = 0
-        self.template_config = ConfigParser()
-        self.current_page = "title"
-        self.current_page_no = 0
-        self.current_pages = []
+        print "Setting selected library to {}".format(library)
+        if library in self.libraries.keys():
+            self.selected_library = library
+        else:
+            self.selected_library = None
 
     def build(self):
         self.title = 'Story Pixies'
@@ -140,23 +335,7 @@ class StoryPixiesApp(App):
 
         return self.top_grid
 
-    def build_config(self, config):
-        config.setdefaults('global', {'template_dir': Path(__file__).parents[0].absolute() / "templates"})
-        for library in self.libraries:
-            config.setdefaults(library, {
-                'name': library,
-                'story_dir': Path(__file__).parents[0].absolute() / "libraries" / library,
-                'current_book': ''
-            })
-
-    def build_settings(self, settings):
-        settings.add_json_panel('Global', self.config, data=get_settings_json('global'))
-        for library in self.libraries:
-            settings.add_json_panel(library.capitalize(), self.config, data=get_settings_json(library))
-
-    def on_config_change(self, config, section, key, value):
-        print config, section, key, value
-
+    # Screen switching methods
     def home_screen(self, button):
         button = self.menu.homebutton
         self.switch_screen('home', button)
@@ -175,123 +354,9 @@ class StoryPixiesApp(App):
 
     def switch_screen(self, screen_name, button):
         print "Switching"
-        #if self.manager and screen_name in self.manager.screen_names:
         button.state = 'down'
 
         self.manager.current = screen_name
-
-    def set_selected_library(self, library):
-        """
-        Resets the defaults.
-        Stores passed library as selected.
-        Gets stories for this library.
-        Sets selected story to the first.
-        :param library: the library to set as current
-        """
-        print "Setting selected library to {}".format(library)
-        if library in self.libraries:
-            self.set_property_defaults()
-            self.selected_library = library
-            self.stories = self.get_stories(self.selected_library)
-            self.set_selected_story(0)
-
-    def set_selected_story(self, num):
-        """
-        Mods passed number so it wraps selected story num.
-        Sets the selected story and story number.
-        Sets the current page to the title page.
-        Sets the current page number to 0.
-        Parses configs using template and library.
-        Sets current pages.
-        :param num:
-        :return: Returns name of selected story.
-        """
-        num %= len(self.stories)
-
-        self.selected_story = self.stories[num]
-        self.selected_story_no = num
-        self.current_page = "title"
-        self.current_page_no = 0
-        self.parse_story_configs()
-
-        # Set the pages in the story
-        pages = self.template_config.get('title', 'pages')
-        self.current_pages = ['title'] + [x.strip() for x in pages.split(',')]
-        return self.template_config.get('title', 'name')
-
-    def parse_story_configs(self):
-        """
-        Parses story configs based on its template. It will interpolate individual
-        story defaults into templatized config.
-        """
-        # Get individual story defaults
-        story_dir = (Path(__file__).parents[0].absolute() / "libraries" / self.selected_library)
-        story_config = ConfigParser()
-        story_config.read(str(story_dir) + '/' + self.selected_story + '.ini')
-
-        # Get templatized values
-        template_dir = self.config.get('global', 'template_dir')
-        self.template_config = ConfigParser()
-
-        # Interpolate individual story defaults into templatized config
-        self.template_config.setall('DEFAULT', dict(story_config.items('values')))
-        self.template_config.read(template_dir + '/' + self.selected_story + '.ini')
-
-    def next_story(self):
-        """
-        Updates the selected story to be the next one.
-        :return: The name of the story
-        """
-        return self.set_selected_story(self.selected_story_no + 1)
-
-    def prev_story(self):
-        """
-        Updates the selected story to be the previous one.
-        :return: The name of the story
-        """
-        return self.set_selected_story(self.selected_story_no - 1)
-
-    def next_page(self):
-        """
-        Updates the current page to be the next page
-        """
-        self.current_page_no = min(self.current_page_no + 1, len(self.current_pages) - 1)
-        self.current_page = self.current_pages[self.current_page_no]
-
-    def previous_page(self):
-        """
-        Updates the current page to be the previous page
-        """
-        self.current_page_no = min(self.current_page_no - 1, len(self.current_pages) - 1)
-        self.current_page = self.current_pages[self.current_page_no]
-
-    @staticmethod
-    def get_libraries():
-        library_list = (Path(__file__).parents[0].absolute() / "libraries")
-        return [l.stem for l in library_list.iterdir() if l.is_dir()]
-
-    def get_stories(self, library=None):
-        if library is None:
-            library = self.selected_library
-
-        story_list = (Path(__file__).parents[0].absolute() / "libraries" / library)
-        return [s.stem for s in story_list.iterdir() if s.is_file()]
-
-    def get_story_value(self, page, value):
-        return self.template_config.get(page, value)
-
-    def get_story_title(self):
-        print "Getting title"
-        return self.get_story_value('title', 'name')
-
-    def get_story_media(self):
-        return self.get_story_value(self.current_page, 'media_location')
-
-    def get_story_media_type(self):
-        return self.get_story_value(self.current_page, 'media')
-
-    def get_story_text(self):
-        return self.get_story_value(self.current_page, 'text')
 
 
 if __name__ == '__main__':
